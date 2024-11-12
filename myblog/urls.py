8153d7802c6c0226772c2a8f3e635eb5daf296b0
Hichama <hicham.afilali@outlook.com>
2024-11-12 15:41:28 +0200

    Finish django configurations

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,8 +1,9 @@
-"""
-URL configuration for myblog project.
+import blog.views
+
+"""blango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +15,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-import blog.views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
